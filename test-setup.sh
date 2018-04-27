fe2fea3bf0185cc38182bab41313fcd9fd2cd88b
Andrew Smith <a.smith@silentworks.co.uk>
2018-04-27 11:46:37 +0000

    Add new test script

--- test-setup.sh
+++ test-setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd slydepay-php-connector
-
-php -v
-
-composer -V
-
-composer install --prefer-dist
+php -v && composer -V && composer install --prefer-dist
 
 ./vendor/bin/kahlan --cc --reporter=bar
